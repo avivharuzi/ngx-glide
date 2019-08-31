f7e32e53cdf8c5a1938f27eb8be0786ca142c514
Aviv Haruzi <avivharuzi@gmail.com>
2019-08-31 12:04:40 +0300

    Add option interface

---
+++ projects/ngx-glide/src/lib/shared/option.interface.ts
@@ -0,0 +1,25 @@
+export interface Option {
+  type: string;
+  startAt: number;
+  perView: number;
+  focusAt: number | string;
+  gap: number;
+  autoplay: number | boolean;
+  hoverpause: boolean;
+  keyboard: boolean;
+  bound: boolean;
+  swipeThreshold: number | boolean;
+  dragThreshold: number | boolean;
+  perTouch: number | boolean;
+  touchRatio: number;
+  touchAngle: number;
+  animationDuration: number;
+  rewind: boolean;
+  rewindDuration: number;
+  animationTimingFunc: string;
+  direction: string;
+  peek: number | object;
+  breakpoints: object;
+  classes: object;
+  throttle: number;
+}
