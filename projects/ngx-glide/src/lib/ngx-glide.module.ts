48495781de38e06f2288988de6db4cba16733146
Aviv Haruzi <avivharuzi@gmail.com>
2019-08-31 15:49:58 +0300

    Update ngx-glide module

--- projects/ngx-glide/src/lib/ngx-glide.module.ts
+++ projects/ngx-glide/src/lib/ngx-glide.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { NgxGlideComponent } from './ngx-glide.component';
 
 @NgModule({
   declarations: [NgxGlideComponent],
   imports: [
+    BrowserModule
   ],
   exports: [NgxGlideComponent]
 })
